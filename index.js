8664992bcd2e0ba505426aa7bc32591dca04595d
sofyan <comnex638@gmail.com>
2023-12-20 17:01:02 +0700

    cors

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ import dotenv from "dotenv";
 import cors from "cors";
 import session from "express-session";
 
-
 dotenv.config();
 
 // route
 import adminRoute from "./routers/adminRoute.js";
 import authRoute from "./routers/authRoute.js";
 import userRoute from "./routers/userRoute.js";
-
 
 mongoose.connect(process.env.DATABASE_URL);
 
@@ -24,24 +22,31 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+// Gunakan middleware CORS di sini
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
   next();
 });
+
 app.use(session({
-  name: process.env.COOKIE, // Nama sesi cookie
-  secret: process.env.SECRET_KEY,     // Kunci rahasia untuk penandatanganan sesi
+  name: process.env.COOKIE,
+  secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: false,    // Atur ke true jika hanya menggunakan HTTPS
-    httpOnly: true,   // Tidak dapat diakses oleh JavaScript di sisi klien
-    maxAge: 3600000,  // Waktu kadaluarsa dalam milidetik (contoh: 1 jam)
-    sameSite: 'strict' // Atur ke 'strict' untuk keamanan sesi yang lebih tinggi
+    secure: false,
+    httpOnly: true,
+    maxAge: 3600000,
+    sameSite: 'strict'
   }
 }));
-
 
 app.use(adminRoute);
 app.use(authRoute);
