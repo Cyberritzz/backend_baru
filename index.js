4bfc565b632aeb447a44b3777dd2e6e5f25f7507
sofyan <comnex638@gmail.com>
2023-12-20 17:52:39 +0700

    cors lagi

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(cors());
-app.use(cors({ credentials: true, origin: 'http://localhost:3001' }));
+app.use(cors({ credentials: true, origin: '*' }));
 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*'); // Atur domain Anda sendiri jika memungkinkan
