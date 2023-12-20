4e8a5e7075aa45641155c450461da9d12976b1af
haris <104260198+Cyberritzz@users.noreply.github.com>
2023-12-20 18:11:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,14 +25,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // app.use(cors());
 //app.use(cors({ credentials: true, origin: 'https://localhost:3001' }));
-app.use(cors({ credentials: true, origin: 'https://admin-uistellar.vercel.app' }));
+// app.use(cors({ credentials: true, origin: 'https://admin-uistellar.vercel.app' }));
 
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); // Atur domain Anda sendiri jika memungkinkan
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.use(cors({
+  origin: ['http://localhost:3001', 'https://admin-uistellar.vercel.app'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
 app.use(session({
   name: process.env.COOKIE,
