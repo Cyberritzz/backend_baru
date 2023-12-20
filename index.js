d5603f6aa3cd6d59317e5e8a68bdf3ead7fc8b87
haris <104260198+Cyberritzz@users.noreply.github.com>
2023-12-20 18:06:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(cors());
-app.use(cors({ credentials: true, origin: 'https://localhost:3001' }));
+//app.use(cors({ credentials: true, origin: 'https://localhost:3001' }));
 app.use(cors({ credentials: true, origin: 'https://admin-uistellar.vercel.app' }));
 
 
