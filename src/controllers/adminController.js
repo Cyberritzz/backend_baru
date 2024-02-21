67e6cb85cda96254adc2118c5eb0e5a858cfd382
unknown <muizzuddin334@gmail.com>
2024-02-21 11:39:57 +0700

    add test for admin get all product and user

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,11 +1,11 @@
 import ProductCol from "../model/productCol.js";
 import UserCol from "../model/userCol.js";
-import cloudinary from 'cloudinary';
+import cloudinary from "cloudinary";
 
 cloudinary.config({
-    cloud_name: 'dpemgsyje',
-    api_key: '536212312211878',
-    api_secret: 'J128DxIZoJ4T9ncC_c_VAe8_1Yc'
+  cloud_name: "dpemgsyje",
+  api_key: "536212312211878",
+  api_secret: "J128DxIZoJ4T9ncC_c_VAe8_1Yc",
 });
 
 const adminController = {
@@ -16,34 +16,33 @@ const adminController = {
       res.send({ message: error.message });
     }
   },
-  getProduk: async (req, res) => {
+  getProduk: async (req, res, next) => {
     try {
-      const {limit, offset} = req.query;
-      const result = await ProductCol.find().limit(limit).skip(offset);
+      const limit = req.query.limit || 0;
+      const offset = req.query.offset || 0;
+      const result = await ProductCol.find().skip(offset).limit(limit);
 
-      if (!result) {
-        res.status(404).send({ message: "data not found" });
-      }
-
-      res.json(result);
+      res.status(200).json(result);
     } catch (error) {
-      res.status(500).send({ message: error.message });
+      next(error);
     }
   },
 
   updateProduk: async (req, res) => {
     try {
       let id = req.params.id;
-      const { name_product, 
-        description, 
-        category, 
+      const {
+        name_product,
+        description,
+        category,
         type_product,
         thumbnail_public_id,
-        source_file_public_id, 
+        source_file_public_id,
         signatureThumbnail,
         signatureSourceFile,
         thumbnail,
-        source_file } = req.body;
+        source_file,
+      } = req.body;
 
       const result = await ProductCol.updateOne(
         { _id: id },
@@ -58,7 +57,7 @@ const adminController = {
             signatureThumbnail,
             signatureSourceFile,
             source_file,
-            thumbnail
+            thumbnail,
           },
         }
       );
@@ -74,14 +73,14 @@ const adminController = {
   updateFoto: async (req, res) => {
     try {
       const id = req.params.id;
-      const thumbnail = req.body.thumbnail
+      const thumbnail = req.body.thumbnail;
 
       const result = await ProductCol.updateOne(
-        {_id : id},
+        { _id: id },
         {
-          $set:{
-            thumbnail
-          }
+          $set: {
+            thumbnail,
+          },
         }
       );
 
@@ -99,11 +98,11 @@ const adminController = {
       const source_file = req.body.source_file;
 
       const result = await ProductCol.updateOne(
-        {_id : id},
+        { _id: id },
         {
-          $set:{
-            source_file
-          }
+          $set: {
+            source_file,
+          },
         }
       );
 
@@ -119,17 +118,18 @@ const adminController = {
 
   uploadProduk: async (req, res) => {
     try {
-      const { 
-        name_product, 
-        description, 
-        category, 
-        type_product, 
-        thumbnail, 
+      const {
+        name_product,
+        description,
+        category,
+        type_product,
+        thumbnail,
         source_file,
         signatureThumbnail,
         signatureSourceFile,
         thumbnail_public_id,
-        source_file_public_id } = req.body;
+        source_file_public_id,
+      } = req.body;
 
       const data = new ProductCol({
         name_product: name_product,
@@ -138,10 +138,10 @@ const adminController = {
         type_product: type_product,
         thumbnail: thumbnail,
         source_file: source_file,
-        signatureThumbnail : signatureThumbnail,
-        signatureSourceFile : signatureSourceFile,
-        thumbnail_public_id : thumbnail_public_id,
-        source_file_public_id : source_file_public_id
+        signatureThumbnail: signatureThumbnail,
+        signatureSourceFile: signatureSourceFile,
+        thumbnail_public_id: thumbnail_public_id,
+        source_file_public_id: source_file_public_id,
       });
 
       await data.save();
@@ -152,16 +152,15 @@ const adminController = {
     }
   },
 
-  getUsers: async (req, res) => {
+  getUsers: async (req, res, next) => {
     try {
-      const {limit, offset} = req.query;
-      const result = await UserCol.find().limit(limit).skip(offset);
-      if (!result) {
-        res.status(404).send({ message: "data not found" });
-      }
+      const limit = req.query.limit || 0;
+      const offset = req.query.offset || 0;
+      const result = await UserCol.find().skip(offset).limit(limit);
+
       res.status(200).json(result);
     } catch (error) {
-      res.status(500).send({ message: error.message });
+      next(error);
     }
   },
 
@@ -171,11 +170,11 @@ const adminController = {
       let is_membership = req.body.is_membership;
 
       const result = await UserCol.findOneAndUpdate(
-        {_id : id},
+        { _id: id },
         {
-          $set : {
-            is_membership : is_membership
-          }
+          $set: {
+            is_membership: is_membership,
+          },
         }
       );
       if (result.modifiedCount === 0) {
@@ -188,7 +187,7 @@ const adminController = {
     }
   },
 
-  deleteProductById : async (req, res) => {
+  deleteProductById: async (req, res) => {
     try {
       const filter = req.params.id;
       const result = await ProductCol.deleteOne({ _id: filter });
@@ -197,59 +196,62 @@ const adminController = {
         return res.status(401).send({ message: "Delete Failed" });
       }
 
-      res.status(200).json({message : 'delete success'});
+      res.status(200).json({ message: "delete success" });
     } catch (error) {
-      res.status(500).json({message: error.message});
+      res.status(500).json({ message: error.message });
     }
   },
 
-  deleteUserById : async (req, res) => {
+  deleteUserById: async (req, res) => {
     try {
       const filter = req.params.id;
-      
+
       // delete user
       const result = await UserCol.deleteOne({ _id: filter });
 
-      if(result.deletedCount === 0) {
+      if (result.deletedCount === 0) {
         return res.status(401).send({ message: "Delete Failed" });
       }
 
-      res.json({message : 'Delete success'});
+      res.json({ message: "Delete success" });
     } catch (error) {
-      res.status(500).json({message: error.message});
+      res.status(500).json({ message: error.message });
     }
   },
 
-  deleteThumbnail : async (req,res) =>{
+  deleteThumbnail: async (req, res) => {
     try {
       const public_id = req.query.public_id;
 
-      cloudinary.v2.uploader.destroy(public_id, function(error, result){
-        if(result){
-          return res.send(result)
+      cloudinary.v2.uploader.destroy(public_id, function (error, result) {
+        if (result) {
+          return res.send(result);
         }
-        res.send({message : error})
-      })
+        res.send({ message: error });
+      });
     } catch (error) {
-      res.status(500).send({message : error.message})
+      res.status(500).send({ message: error.message });
     }
   },
 
-  deleteRawFile : async (req,res) =>{
+  deleteRawFile: async (req, res) => {
     try {
       const public_id = req.query.public_id;
 
-      cloudinary.v2.uploader.destroy(public_id, { invalidate: true, resource_type: "raw"}, function(error, result){
-        if(result){
-          return res.send(result)
+      cloudinary.v2.uploader.destroy(
+        public_id,
+        { invalidate: true, resource_type: "raw" },
+        function (error, result) {
+          if (result) {
+            return res.send(result);
+          }
+          res.send({ message: error });
         }
-        res.send({message : error})
-      })
+      );
     } catch (error) {
-      res.status(500).send({message : error.message})
+      res.status(500).send({ message: error.message });
     }
   },
-  
 };
 
 export default adminController;
